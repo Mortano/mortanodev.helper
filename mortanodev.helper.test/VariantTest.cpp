d0fd44124236f4de9b9c6e51a077cacb242e17e3
Mortano <mortano@hotmail.de>
2015-10-11 18:32:45 +0200

    Tests for Variant with mixed primitive and non-pod types

--- mortanodev.helper.test/VariantTest.cpp
+++ mortanodev.helper.test/VariantTest.cpp
@@ -284,7 +284,7 @@ namespace mortanodevhelpertest
          Assert::IsFalse(v3.Is<float>());
       }
 
-      TEST_METHOD(Test_TwoPrimitives_CopyAssing)
+      TEST_METHOD(Test_TwoPrimitives_CopyAssign)
       {
          using Var_t = mdv::Variant<int, float>;
 
@@ -392,6 +392,211 @@ namespace mortanodevhelpertest
          Assert::IsFalse(v2.HasValue());
          Assert::IsFalse(v2.Is<int>());
          Assert::IsFalse(v2.Is<float>());
+      }
+
+      TEST_METHOD(Test_TwoMixed_DefaultCtor)
+      {
+         using Var_t = mdv::Variant<int, std::string>;
+
+         Var_t v;
+
+         Assert::IsFalse(v.HasValue());
+         Assert::IsFalse(v.Is<int>());
+         Assert::IsFalse(v.Is<std::string>());
+         Assert::ExpectException<std::exception>([&v]() { v.Get<int>(); });
+         Assert::ExpectException<std::exception>([&v]() { v.Get<std::string>(); });
+      }
+
+      TEST_METHOD(Test_TwoMixed_ArgCtor)
+      {
+         using namespace std::string_literals;
+         using Var_t = mdv::Variant<int, std::string>;
+
+         Var_t v1(42);
+
+         Assert::IsTrue(v1.HasValue());
+         Assert::IsTrue(v1.Is<int>());
+         Assert::AreEqual(42, v1.Get<int>());
+
+         Var_t v2("Hello"s);
+
+         Assert::IsTrue(v2.HasValue());
+         Assert::IsTrue(v2.Is<std::string>());
+         Assert::AreEqual("Hello"s, v2.Get<std::string>());
+      }
+
+      TEST_METHOD(Test_TwoMixed_CopyCtor)
+      {
+         using namespace std::string_literals;
+         using Var_t = mdv::Variant<int, std::string>;
+
+         Var_t v1(42);
+         Var_t v2(v1);
+
+         Assert::AreEqual(v1.HasValue(), v2.HasValue());
+         Assert::AreEqual(v1.Is<int>(), v2.Is<int>());
+         Assert::AreEqual(v1.Get<int>(), v2.Get<int>());
+
+         Var_t v3("Hello"s);
+         Var_t v4(v3);
+
+         Assert::AreEqual(v3.HasValue(), v4.HasValue());
+         Assert::AreEqual(v3.Is<std::string>(), v4.Is<std::string>());
+         Assert::AreEqual(v3.Get<std::string>(), v4.Get<std::string>());
+      }
+
+      TEST_METHOD(Test_TwoMixed_CopyFromEmpty)
+      {
+         using Var_t = mdv::Variant<int, std::string>;
+
+         Var_t v1;
+         Var_t v2(v1);
+
+         Assert::AreEqual(v1.HasValue(), v2.HasValue());
+         Assert::AreEqual(v1.Is<int>(), v2.Is<int>());
+      }
+
+      TEST_METHOD(Test_TwoMixed_MoveCtor)
+      {
+         using namespace std::string_literals;
+         using Var_t = mdv::Variant<int, std::string>;
+
+         Var_t v1(42);
+         Var_t v2(std::move(v1));
+
+         Assert::IsTrue(v2.HasValue());
+         Assert::IsTrue(v2.Is<int>());
+         Assert::AreEqual(42, v2.Get<int>());
+
+         Assert::IsFalse(v1.HasValue());
+         Assert::IsFalse(v1.Is<int>());
+
+         Var_t v3("Hello"s);
+         Var_t v4(std::move(v3));
+
+         Assert::IsTrue(v4.HasValue());
+         Assert::IsTrue(v4.Is<std::string>());
+         Assert::AreEqual("Hello"s, v4.Get<std::string>());
+
+         Assert::IsFalse(v3.HasValue());
+         Assert::IsFalse(v3.Is<std::string>());
+      }
+
+      TEST_METHOD(Test_TwoMixed_CopyAssign)
+      {
+         using namespace std::string_literals;
+         using Var_t = mdv::Variant<int, std::string>;
+
+         Var_t v1(42);
+         Var_t v2(23);
+
+         v2 = v1;
+
+         Assert::AreEqual(v1.HasValue(), v2.HasValue());
+         Assert::AreEqual(v1.Is<int>(), v2.Is<int>());
+         Assert::AreEqual(v1.Get<int>(), v2.Get<int>());
+
+         Var_t v3("Hello"s);
+         Var_t v4("HansWurst"s);
+
+         v4 = v3;
+
+         Assert::AreEqual(v3.HasValue(), v4.HasValue());
+         Assert::AreEqual(v3.Is<std::string>(), v4.Is<std::string>());
+         Assert::AreEqual(v3.Get<std::string>(), v4.Get<std::string>());
+
+         //Cross type assign
+         Var_t v5("Hello"s);
+         Var_t v6(42);
+
+         v6 = v5;
+
+         Assert::AreEqual(v5.HasValue(), v6.HasValue());
+         Assert::AreEqual(v5.Is<std::string>(), v6.Is<std::string>());
+         Assert::AreEqual(v5.Get<std::string>(), v6.Get<std::string>());
+      }
+
+      TEST_METHOD(Test_TwoMixed_MoveAssign)
+      {
+         using namespace std::string_literals;
+         using Var_t = mdv::Variant<int, std::string>;
+
+         Var_t v1(42);
+         Var_t v2(23);
+
+         v2 = std::move(v1);
+
+         Assert::IsTrue(v2.HasValue());
+         Assert::IsTrue(v2.Is<int>());
+         Assert::AreEqual(42, v2.Get<int>());
+
+         Assert::IsFalse(v1.HasValue());
+         Assert::IsFalse(v1.Is<int>());
+
+         Var_t v3("Hello"s);
+         Var_t v4("HansWurst"s);
+
+         v4 = std::move(v3);
+
+         Assert::IsTrue(v4.HasValue());
+         Assert::IsTrue(v4.Is<std::string>());
+         Assert::AreEqual("Hello"s, v4.Get<std::string>());
+
+         Assert::IsFalse(v3.HasValue());
+         Assert::IsFalse(v3.Is<std::string>());
+
+         //Cross type assign
+         Var_t v5("Hello"s);
+         Var_t v6(42);
+
+         v6 = std::move(v5);
+
+         Assert::IsTrue(v6.HasValue());
+         Assert::IsTrue(v6.Is<std::string>());
+         Assert::AreEqual("Hello"s, v6.Get<std::string>());
+
+         Assert::IsFalse(v5.HasValue());
+         Assert::IsFalse(v5.Is<std::string>());
+      }
+
+      TEST_METHOD(Test_TwoMixed_ValueAssign)
+      {
+         using namespace std::string_literals;
+         using Var_t = mdv::Variant<int, std::string>;
+
+         Var_t v1(23);
+
+         v1 = "Hello"s;
+
+         Assert::IsTrue(v1.HasValue());
+         Assert::IsTrue(v1.Is<std::string>());
+         Assert::AreEqual("Hello"s, v1.Get<std::string>());
+
+         v1 = 42;
+
+         Assert::IsTrue(v1.HasValue());
+         Assert::IsTrue(v1.Is<int>());
+         Assert::AreEqual(42, v1.Get<int>());
+      }
+
+      TEST_METHOD(Test_TwoMixed_Clear)
+      {
+         using namespace std::string_literals;
+         using Var_t = mdv::Variant<int, std::string>;
+
+         Var_t v1(42);
+
+         v1.Clear();
+
+         Assert::IsFalse(v1.HasValue());
+         Assert::IsFalse(v1.Is<int>());
+
+         v1 = "Hello"s;
+
+         v1.Clear();
+
+         Assert::IsFalse(v1.HasValue());
+         Assert::IsFalse(v1.Is<std::string>());
       }
 
 	};
